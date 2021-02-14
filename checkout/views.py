36f4420dbb859b4d1b641f289ab904ca04d8ab38
jakefernihough <jakefernihough@gmail.com>
2021-02-14 18:18:01 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,9 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': (
+            'pk_test_51IKojnJMFfRBmWPrbWmCiuXyaaiHwEOr2CXFWh7qT64lJrbSZidDcOEi3KtCJv0GDdMj6og9lbIohKDPywZnj2Ko00vlWLkR3r'),
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
--- memberships/views.py
+++ memberships/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 from .models import Membership
 
