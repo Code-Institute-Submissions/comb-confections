cdcc55018d35bc7a2abe138536fe0173aadde7f9
jakefernihough <jakefernihough@gmail.com>
2021-02-20 16:24:18 +0000

    added and styled membership_checkout page

--- memberships/urls.py
+++ memberships/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('membership_type/', views.membership_type,
          name='membership_type'),
     path('membership_checkout/', views.membership_checkout,
-          name="membership_checkout"),
+         name="membership_checkout"),
     path('user_membership/', views.user_membership_view,
-          name="user_membership"),
+         name="user_membership"),
     path('membership_change/', views.membership_change,
          name="membership_change"),
 ]
--- memberships/views.py
+++ memberships/views.py
@@ -1,11 +1,9 @@
-from django.conf import settings
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from .models import Membership
 from profiles.models import UserProfile
 from django.contrib import messages
-from django.contrib.auth.models import User
 
 
 def memberships(request):
@@ -42,33 +40,22 @@ def membership_type(request):
 @login_required
 def membership_checkout(request):
     """
-    Retrieve user selected membership, display it and
-    benefits, and allow user to change the membership
-    type
+    Retrieve user selected membership, display
+    benefits, and allow user to change membership
     """
     # Retrieve data for all memberships
-    all_memberships = Membership.objects.all()
+    types_of_memberships = Membership.objects.all()
 
-    # Check if user already has a memership and got to this
-    # page by accident, then re-direct to change site
     profile = UserProfile.objects.get(user=request.user)
     if profile.membership:
         return redirect(reverse('membership_change'))
-    # If user is updating selected membership, the
-    # memberhip_type to the new value
     if request.GET.get('membership-new'):
         membership_type = request.GET.get('membership-new')
-        # add membership type to session to retrieve for stripe
         request.session['membership'] = membership_type
-    # If user logged in after
-    # registering, get membership_type from session
     else:
         try:
-            # Retrieve user selected membership
             membership_type = request.session['membership']
         except KeyError:
-            # If user logged in normally, redirect them
-            # to the profile page
             return redirect(reverse('products'))
 
     # Retrieve data for selected membership type
@@ -77,7 +64,7 @@ def membership_checkout(request):
     template = 'memberships/membership_checkout.html'
     context = {
         'membership': membership,
-        'all_memberships': all_memberships,
+        'types_of_memberships': types_of_memberships,
     }
 
     return render(request, template, context)
@@ -117,7 +104,7 @@ def membership_change(request):
     if not request.POST.get('membership_type'):
         return redirect(reverse('memberships'))
 
-    all_memberships = Membership.objects.all()
+    types_of_memberships = Membership.objects.all()
     membership_type = request.POST.get('membership_type')
     request.session['membership'] = membership_type
     membership = get_object_or_404(Membership, name=membership_type)
@@ -126,6 +113,6 @@ def membership_change(request):
     context = {
         'change_membership': True,
         'membership': membership,
-        'all_memberships': all_memberships,
+        'types_of_memberships': types_of_memberships,
     }
     return render(request, template, context)
