846a60c8e66d88df0eef96efb3fead879ac8f447
jakefernihough <jakefernihough@gmail.com>
2021-03-05 23:15:09 +0000

    Added cache control

--- comb_confections/settings.py
+++ comb_confections/settings.py
@@ -180,6 +180,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Amazon Web Services
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'comb-confections'
     AWS_S3_REGION_NAME = 'eu-west-2'
